ba8cfe8c7d053930f0514630f1cb1edcf4250879
rifat-54 <rifat999por@gmail.com>
2025-11-20 17:03:18 +0600

    solve problem 4

--- solution.ts
+++ solution.ts
@@ -13,6 +13,7 @@ const formatValue=(value:Value)=>{
 
 
 
+
 type GetLength<T> =string | T[] 
 
 const getLength=<T>(value:GetLength<T>):number | undefined=>{
@@ -27,15 +28,14 @@ const getLength=<T>(value:GetLength<T>):number | undefined=>{
 
 
 
+
 class Person{
     name:string;
     age:number;
-
     constructor(name:string,age:number){
         this.name=name;
         this.age=age;
     }
-
     getDetails(){
         return `'Name: ${this.name}, Age: ${this.age}';`
     }
@@ -43,9 +43,23 @@ class Person{
 }
 
 
-const person1 = new Person('John Doe', 30);
-console.log(person1.getDetails());
 
-const person2 = new Person('Alice', 25);
-console.log(person2.getDetails());
+
+type TFilterByRating={
+    title:string;
+    rating:number;
+}
+
+const filterByRating=(value:TFilterByRating[])=>{
+    return value.filter((item)=>{
+        if(item.rating>=4){
+            return item;
+        }  
+    })
+}
+
+
+
+
+
 
