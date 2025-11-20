2e654aed31cc512928c12b49404fd9d727f10692
rifat-54 <rifat999por@gmail.com>
2025-11-20 17:11:47 +0600

    solve problem 5

--- solution.ts
+++ solution.ts
@@ -59,7 +59,22 @@ const filterByRating=(value:TFilterByRating[])=>{
 }
 
 
+type TFilterActiveUsers={
+    id:number;
+    name:string;
+    email:string;
+    isActive:boolean;
+}
+
+const filterActiveUsers=(value:TFilterActiveUsers[]):TFilterActiveUsers[]=>{
+    return value.filter((item)=>item.isActive)
+}
 
 
+const users = [
+  { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
+  { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
+  { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive:true },
+];
 
-
+console.log(filterActiveUsers(users));
